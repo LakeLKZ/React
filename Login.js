0b2311044d6e99d7e5449756f65a57e6f2d8e8c4
LakeLKZ <100738724+LakeLKZ@users.noreply.github.com>
2022-07-16 16:57:03 -0300

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,16 +1,29 @@
 function Login() {
 
-    const Formulario = e =>{
+    const submitHandler = e =>{
         e.preventDefault();
-        const email = e.target.email;
-        const password = e.target.password;
-        console.log(email, password);
+        const email = e.target.email.value;
+        const password = e.target.password.value;
 
-    }
+        const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if(email === '' || password === ''){
+            console.log("flaco las cosas no pueden estar vacias")
+            return;
+        }
+
+
+        if(email !== '' && !regexEmail.test(email)){
+            console.log("la direccion de correo no es valida, pone otra pls ")
+            return;
+        }        
+        
+        console.log("Ok, estamos listos para mandar la info")
+    }   
     return (
         <>
             <h2>Ey! queres que te robe todos tus datos con este login verdad?</h2>
-            <form onSubmit={Formulario}>
+            <form onSubmit={submitHandler}>
                 <label>
                     <span>Ingrese su Mail</span>
                     <br/>
